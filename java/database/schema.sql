cc235223c835db9eb8adcd45e73c9ba8757b1f9a
Ying Huang <huangying270@gmail.com>
2022-12-02 15:36:36 -0500

    schema

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -10,4 +10,45 @@ CREATE TABLE users (
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
 
+CREATE TABLE decks (
+    deck_id SERIAL,
+    deck_name varchar(50) NOT NULL,
+    deck_description varchar(200),
+    accessible boolean default false,
+    creator_id int NOT NULL,
+    CONSTRAINT PK_deck PRIMARY KEY (deck_id),
+    CONSTRAINT FK_deck_user FOREIGN KEY (creator_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE user_deck (
+    deck_id int NOT NULL,
+    user_id int NOT NULL,
+    CONSTRAINT PK_user_deck PRIMARY KEY (deck_id, user_id),
+    CONSTRAINT FK_user_deck_deck FOREIGN KEY (deck_id) REFERENCES decks(deck_id),
+    CONSTRAINT FK_user_deck_user FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE deck_tag (
+    deck_id int NOT NULL,
+    tag_id int NOT NULL,
+    CONSTRAINT PK_deck_tag PRIMARY KEY (deck_id, tag_id),
+    CONSTRAINT FK_deck_tag_deck FOREIGN KEY (deck_id) REFERENCES decks(deck_id),
+    CONSTRAINT FK_deck_tag_tag FOREIGN KEY (tag_id) REFERENCES tags(tag_id)
+);
+
+CREATE TABLE tags (
+    tag_id SERIAL,
+    tag_name varchar(50) NOT NULL,
+    CONSTRAINT PK_tag PRIMARY KEY (tag_id)
+);
+
+CREATE TABLE cards (
+    card_id SERIAL,
+    deck_id int NOT NULL,
+    front varchar(500) NOT NULL,
+    back varchar(500) NOT NULL,
+    CONSTRAINT PK_card PRIMARY KEY (card_id),
+    CONSTRAINT FK_card_deck FOREIGN KEY (deck_id) REFERENCES decks(deck_id)
+);
+
 COMMIT TRANSACTION;
