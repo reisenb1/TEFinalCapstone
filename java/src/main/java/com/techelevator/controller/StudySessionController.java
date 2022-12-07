1b79b0a1f7a2454860b6f934642db47855d7dd7c
Emily Hamilton <ehamilton617@gmail.com>
2022-12-07 09:52:55 -0500

    changes to studysession

--- java/src/main/java/com/techelevator/controller/StudySessionController.java
+++ java/src/main/java/com/techelevator/controller/StudySessionController.java
@@ -2,9 +2,11 @@ package com.techelevator.controller;
 
 
 import com.techelevator.dao.StudySessionDao;
+import com.techelevator.model.StudySession;
 import com.techelevator.services.StudySessionService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -18,5 +20,32 @@ public class StudySessionController {
         this.studySessionDao = studySessionDao;
         this.studySessionService = studySessionService;
     }
+
+
+    @RequestMapping(path = "/{userId}/studySessions", method = RequestMethod.GET)
+    public List<StudySession> getAllStudySessionsFromUser(@PathVariable int userId) {
+        return studySessionDao.getAllMyStudySessions(userId);
+
+    }
+
+    @RequestMapping(path = "/studySessions/{studySessionId}", method = RequestMethod.GET)
+    public Deck getDeck(@PathVariable int deckId) {
+        return deckDao.getDeck(deckId);
+    };
+
+    @RequestMapping(path = "/decks", method = RequestMethod.POST)
+    public Deck createDeck(@RequestBody Deck deck) {
+        return deckDao.createDeck(deck);
+    }
+
+    @RequestMapping(path = "/decks/{deckId}", method = RequestMethod.PUT)
+    public boolean updateDeck(@RequestBody Deck deck, @PathVariable int deckId) {
+        return deckDao.updateDeck(deck);
+    }
+
+    @RequestMapping(path = "/decks/tags/{tagName}", method = RequestMethod.GET)
+    public Deck getDeckWithTagName(@PathVariable String tagName) {
+        return deckDao.getDeckWithTagName(tagName);
+    };
 
 }
