028e065b3ad6f5ff3bc0e96bc15e472a06afab0b
Ying Huang <huangying270@gmail.com>
2022-12-09 11:17:34 -0500

    color coded percent correct

--- vue/src/components/Results.vue
+++ vue/src/components/Results.vue
@@ -2,15 +2,72 @@
   <div>
     <h1>Deck Name Study Session Results</h1>
     <button>Finish Session</button>
-    <div class="card">
-      <div>
-        You got {{ studySession.numberCorrect }}/{{studySession.numberOfCards}} a percentage of
+
+
+<!-- <div class="percentColorGreen" v-if="studySession.correctPercent >= 70">
+        You got {{ studySession.numberCorrect }}/{{
+          studySession.numberOfCards
+        }}
+        a percentage of
         {{ studySession.correctPercent }}
       </div>
-      <div>
-        You have a confidence score of {{ studySession.confidencePercent }}
+      <div
+        class="percentColorYellow"
+        v-if="
+          studySession.correctPercent >= 50 && studySession.correctPercent < 70
+        "
+      >
+        You got {{ studySession.numberCorrect }}/{{
+          studySession.numberOfCards
+        }}
+        a percentage of
+        {{ studySession.correctPercent }}
       </div>
+      <div class="percentColorRed" v-if="studySession.correctPercent < 50">
+        You got {{ studySession.numberCorrect }}/{{
+          studySession.numberOfCards
+        }}
+        a percentage of
+        {{ studySession.correctPercent }}
+      </div> -->
+
+
+
+
+
+    <div class="card backgroundColorGreen" v-if="studySession.correctPercent >= 70">
+      <div>
+        You got {{ studySession.numberCorrect }}/{{
+          studySession.numberOfCards
+        }}
+        a percentage of
+        {{ studySession.correctPercent }}%
+      </div>
+      <div>Confidence Score: {{ studySession.confidencePercent }}%</div>
     </div>
+
+    <div class="card backgroundColorYellow" v-if="studySession.correctPercent >= 50 && studySession.correctPercent < 70">
+      <div>
+        You got {{ studySession.numberCorrect }}/{{
+          studySession.numberOfCards
+        }}
+        a percentage of
+        {{ studySession.correctPercent }}%
+      </div>
+      <div>Confidence Score: {{ studySession.confidencePercent }}%</div>
+    </div>
+
+    <div class="card backgroundColorRed" v-if="studySession.correctPercent < 50">
+      <div>
+        You got {{ studySession.numberCorrect }}/{{
+          studySession.numberOfCards
+        }}
+        a percentage of
+        {{ studySession.correctPercent }}%
+      </div>
+      <div>Confidence Score: {{ studySession.confidencePercent }}%</div>
+    </div>
+
     <div class="card" v-for="card in cards" v-bind:key="card.cardId">
       <div class="front">{{ card.front }}</div>
       <div class="back">{{ card.back }}</div>
@@ -73,7 +130,7 @@ export default {
           this.isLoading = false;
         })
         .catch((error) => {
-            if (error.response && error.response.status === 404) {
+          if (error.response && error.response.status === 404) {
             alert("Study session not available.");
             this.$router.push({ name: "Deck" });
           }
@@ -88,4 +145,36 @@ export default {
 </script>
 
 <style>
+
+.backgroundColorGreen {
+    background-color: green;
+    font-size: 24px;
+    font-weight: bold;
+}
+
+.backgroundColorYellow {
+    background-color: yellow;
+    font-size: 24px;
+    font-weight: bold;
+}
+
+.backgroundColorRed {
+    background-color: red;
+    font-size: 24px;
+    font-weight: bold;
+}
+
+.percentColorYellow {
+    font-size: 24px;
+  color: yellow;
+  font-weight: bold;
+}
+
+.percentColorGreen {
+  color: green;
+}
+
+.percentColorRed {
+  color: red;
+}
 </style>
