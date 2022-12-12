639b24d368cf1a87e6ad857470737cb06156713b
Emily Hamilton <ehamilton617@gmail.com>
2022-12-12 17:49:37 -0500

    changes to study session

--- vue/src/components/StudySession.vue
+++ vue/src/components/StudySession.vue
@@ -39,9 +39,11 @@
 
         <div id="candy" v-show="showCorrectIncorrectForm">
           <button id="correct" v-on:click.prevent="submitCorrectIncorrect('correct')">
+            <img class="correctIncorrectImg" src="../images/green-checkmark.png" alt="checkmark">
             Correct
           </button>
           <button id="incorrect" v-on:click.prevent="submitCorrectIncorrect('incorrect')">
+             <img class="correctIncorrectImg" src="../images/red-x.png" alt="x">
             Incorrect
           </button>
         </div>
@@ -236,6 +238,12 @@ export default {
 </script>
 
 <style>
+
+.correctIncorrectImg{
+  height: 20px;
+  margin-right: 10px;
+}
+
 #moreButtons {
   display: flex;
   justify-content: space-evenly;
@@ -301,28 +309,32 @@ export default {
 
 
 #correct {
-  align-self: right;
-  justify-self: right;
-  background-color:rgb(0, 255, 0, 0.6);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  /* background-color:rgb(0, 255, 0, 0.3); */
   height: 40px;
   width: 120px;
   font-weight: bold;
   font-size:medium;
-  border: none;
+  border: solid, 1px;
+  border-color: rgb(0,255,0);
   border-radius: 5px;
   box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
   margin-bottom: 25px;
 }
 
 #incorrect {
-  align-self: right;
-  justify-self: right;
-  background-color: rgb(255, 0, 0, 0.6);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  /* background-color: rgb(255, 0, 0, 0.6); */
   height: 40px;
   width: 120px;
   font-weight: bold;
   font-size:medium;
-  border: none;
+  border: solid, 1px;
+  border-color: rgb(255,0,0);
   border-radius: 5px;
   box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
   margin-bottom: 25px;
@@ -377,14 +389,16 @@ export default {
 }
 
 .flip-card-container {
+
   display: inline-block;
-  width: 500px;
+  width: 650px;
   height: 400px;
-/*   border: 1px solid #CCC; */
+  /* border: 1px solid #CCC; */
   margin: 40px 0;
   perspective: 600px;
   border-radius: 10px;
   /* box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19); */
+
 }
 
 .flip-card-inner {
@@ -402,14 +416,20 @@ export default {
 }
 
 .card_face {
+  
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
   position: absolute;
   width: 100%;
   height: 100%;
-  line-height: 260px;
+  /* line-height: 260px; */
   color: white;
   text-align: center;
+
   font-weight: bold;
-  font-size: 40px;
+  font-size: 30px;
   backface-visibility: hidden;
 }
 
